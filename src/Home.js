ad6a7c8efcdbd8b677022c4691cc391fb54c4ee6
Deepak <panchaldeepak49@gmail.com>
2023-04-07 00:07:31 +0530

    my first commit for this

---
+++ src/Home.js
@@ -0,0 +1,183 @@
+import React from 'react'
+
+import './home.css'
+
+
+const Home = () => {
+  return (
+    <>
+    <div className='main'>
+        
+        
+        <div className="div1">
+            <div className='ml-5 mt-4 font-bold text-rose-400 text-lg font-sans'>Hira R.</div>
+            <div className='ml-5 text-gray-300'>UX/UI Designer</div>
+            <div className='flex mt-7 ml-4 '>
+            <div>
+                <div className='text-white text-sm'>125</div>
+                <div className='text-gray-300 text-sm'>Projects</div>
+            </div>
+            <div className='ml-4'>
+                <div className='text-white text-sm'>$124</div>
+                <div className='text-gray-300 text-sm'>Revenue</div>
+            </div>
+            </div>
+        </div>
+        <div className="div2">
+            <div className='mr-10 mt-2 float-right text-white'>
+            <p className='text-gray-300'>Total Revenue</p>
+            <p className='font-bold text-white'>$92,983</p>
+            <div className='flex'>
+            <p className='h-1.5 w-20 bg-blue-700 mt-2 rounded-2xl'></p>
+            <p className='h-1.5 w-8 bg-white mt-2 rounded-2xl'></p>
+            </div>
+            </div>
+            <div className='flex justify-center items-center mt-6 ml-7 h-9 w-9 bg-black rounded-2xl font-bold text-white'>$</div>
+            </div>
+
+        <div className="div3 flex justify-evenly ">
+            <div className='mt-10'>icon</div>
+            <div className='text-white'>
+                <div className='flex'>
+            <p className='mt-5 font-bold text-lg font-mono'>Dear Client</p>
+            <i class="fa-solid fa-angle-down mt-7 ml-20"></i>
+            </div>
+                <p className='mt-2 text-sm text-gray-300'>Join our online community.It helps</p>
+                <p className='text-sm text-gray-300'>in managing your projects.</p>
+                </div>
+            
+        </div>
+        <div className="div4">
+            <p className='ml-5 text-white mt-5'><i class="fa-regular fa-table-columns"/><span className='ml-5'>Dashboard</span></p>
+            <p className='ml-5 text-gray-300 mt-5'><i class="fa-sharp fa-light fa-envelope"/><span className='ml-5'>Email</span></p>
+            <p className='ml-5 text-gray-300 mt-3'><i class="fa-light fa-calendar"/><span className='ml-5'>Calender</span></p>
+            <p className='ml-5 text-gray-300 mt-3'><i class="fa-sharp fa-regular fa-chart-simple"/><span className='ml-5'>Graph</span></p>
+            <p className='ml-5 text-gray-300 mt-3'><i class="fa-brands fa-wpforms"/><span className='ml-5'>Forms</span></p>
+        </div>
+        <div className="div5">
+            <p className='ml-4 mt-5 font-bold text-white'>Travel</p>
+            <p className='ml-4 text-gray-300 text-sm'>Mobile Application</p>
+            <p className='ml-4 mt-2 text-gray-300 text-xs'>Scan by</p>
+            <div className='flex  mt-10'>
+            <p className='flex justify-center items-center h-5 w-12 bg-blue-700 text-gray-300 text-xs ml-4 rounded-lg '>Tour</p>
+            <p className='flex justify-center items-center h-5 w-12 bg-black text-gray-300 text-xs ml-3 rounded-lg'>Trip</p>
+            </div>
+            <p className='flex justify-center items-center h-5 w-20 bg-black text-gray-300 text-xs ml-4 mt-2 rounded-lg'>Transport</p>
+        </div>
+        <div className="div6">
+        <p className='ml-4 mt-5 font-bold text-white'>Hotel</p>
+            <p className='ml-4 text-gray-300 text-sm'>Dashboard</p>
+            <p className='ml-4 mt-2 text-gray-300 text-xs'>Scan by</p>
+            <div className='flex  mt-10'>
+            <p className='flex justify-center items-center h-5 w-12 bg-blue-700 text-gray-300 text-xs ml-4 rounded-lg '>Tour</p>
+            <p className='flex justify-center items-center h-5 w-12 bg-black text-gray-300 text-xs ml-3 rounded-lg'>Trip</p>
+            </div>
+            <p className='flex justify-center items-center h-5 w-20 bg-black text-gray-300 text-xs ml-4 mt-2 rounded-lg'>Transport</p>
+        </div>
+
+        <div className="div7">
+        <div className='flex justify-between'>
+        <p className='ml-5 mt-5 font-bold text-white'>Transaction Detail</p>
+        <img src='Images/pim2.jpg' alt='missing' className='w-10 h-10 rounded-3xl  mt-5 mr-8'></img>
+        </div>
+        <p className='ml-5 text-gray-200'>Hi Hira</p>
+        <div className='flex'>
+        <p className='ml-5 mt-3 font-bold text-white'>$4,763.40</p>
+        <p className='text-sm text-gray-300 ml-56 mt-4'>...3863</p>
+        </div>
+        
+        <p className='ml-5 text-gray-300 text-xs'>Available Balance</p>
+        
+        <div className='flex '>
+        <p className='flex justify-center items-center ml-7 mt-7 h-8 w-8 bg-black rounded-2xl'>
+        <i class="fa-solid fa-arrow-right text-white"></i></p>
+        <img src='Images/pim1.jpg' alt='missing' className='w-9 h-9 rounded-3xl mt-7 ml-3'></img>
+        <img src='Images/pim2.jpg' alt='missing' className='w-9 h-9 rounded-3xl mt-7 ml-3'></img>
+        <img src='Images/pim3.jpg' alt='missing' className='w-9 h-9 rounded-3xl mt-7 ml-3'></img>
+        <img src='Images/pim1.jpg' alt='missing' className='w-9 h-9 rounded-3xl mt-7 ml-3'></img>
+        </div>
+        <div className='flex justify-center items-center h-7 w-20 bg-rose-400 font-bold text-white text-sm float-right mr-5 mt-2 rounded-2xl'>Send</div>
+        </div>
+
+        <div className="div8">
+            <div className='mt-5 ml-5 font-bold text-white text-lg'>Go Pro</div>
+            <p className='ml-5 mt-2 text-white text-sm'>Update your plan to</p>
+            <p className='ml-5 text-white text-sm'>get pro benefits</p>
+            <p className='flex justify-center items-center ml-20 mt-4 text-white text-sm h-8 w-20 bg-black rounded-tl-xl rounded-b-xl'>Let's Start</p>
+        </div>
+        <div className="div9">
+            <div className='flex'>
+            <p className='ml-5 mt-5 font-bold text-white'>Free slots available</p>
+            <p className='ml-16 mt-7 h-1 w-7 bg-black rounded-lg'></p>
+            <p className='mt-6 h-3 w-3 bg-blue-700 rounded-lg'></p>
+            </div>
+            <div className='flex justify-evenly mt-5'>
+                <p className='h-20 w-16 bg-rose-400 rounded-lg'>
+                    <p className='flex justify-center mt-4 font-bold text-white'>12</p>
+                    <p className='flex justify-center text-white'>June</p>
+                </p>
+                <p className='h-20 w-16 bg-black rounded-lg'>
+                    <p className='flex justify-center mt-4 font-bold text-white'>18</p>
+                    <p className='flex justify-center text-gray-300'>June</p>
+                </p>
+                <p className='h-20 w-16 bg-black rounded-lg'>
+                    <p className='flex justify-center mt-4 font-bold text-white'>20</p>
+                    <p className='flex justify-center text-gray-300'>June</p>
+                </p>    
+            </div>
+
+            <div className='flex justify-evenly mt-5'>
+                <p className='h-20 w-16 bg-black rounded-lg'>
+                    <p className='flex justify-center mt-4 font-bold text-white'>2</p>
+                    <p className='flex justify-center text-gray-300'>July</p>
+                </p>
+                <p className='h-20 w-16 bg-black rounded-lg'>
+                    <p className='flex justify-center mt-4 font-bold text-white'>10</p>
+                    <p className='flex justify-center text-gray-300'>July</p>
+                </p>
+                <p className='h-20 w-16 bg-black rounded-lg'>
+                    <p className='flex justify-center mt-4 font-bold text-white'>15</p>
+                    <p className='flex justify-center text-gray-300'>July</p>
+                </p>    
+            </div>
+
+            </div>
+        <div className="div10">
+        <div className='flex justify-between'>
+        <p className='ml-5 mt-4 text-gray-300'>Project Details</p>
+        <p className='mr-3 mt-4 font-bold text-white rotate-90'>...</p>
+        </div>
+        <p className='ml-5 font-bold text-rose-400 '>Hira R.</p>
+        <p className='ml-5 text-gray-300 text-xs'>UI/UX Designer</p>
+        <p className='ml-5 mt-4 text-white text-sm'>Projects Progress</p>
+        <p className='text-white ml-40'>75%</p>
+        <div className='flex'>
+        <p className='ml-5 mt-1 h-2 w-28 bg-blue-700 rounded-2xl'></p>
+        <p className='mt-1 h-2 w-10 bg-white rounded-2xl'></p>
+        </div>
+        </div>
+
+        <div className="div11">
+        <div className='flex justify-between'>
+        <p className='ml-5 mt-4 text-white text-sm'>Projects Progress</p>
+        <p className='mr-3 mt-4 font-bold text-white rotate-90'>...</p>
+        </div>
+        <p className='flex justify-center items-center ml-10 shadow-xl h-24 w-24 rounded-full'>
+        <p className='flex justify-center items-center  h-20 w-20 rounded-full border-8 border-indigo-600'>   
+        <p className='flex justify-center items-center h-12 w-12 bg-rose-400 rounded-full font-bold text-white'>75%</p>
+        </p>
+        </p> 
+        
+        
+        <p className='ml-5 mt-2 text-gray-300 text-xs'>3 Remaining Projects</p>
+        
+        </div>
+        
+        
+    </div>
+
+    </> 
+  )
+}
+
+export default Home
